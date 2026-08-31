c9fac463e7d9b470888e4951bd2aab0e505114c5
Kasireddy Saiteja Reddy <kasireddysaitejareddy@gmail.com>
2026-08-31 21:07:38 +0530

    files are added

--- OneandTwo.java
+++ OneandTwo.java
@@ -3,34 +3,39 @@ import java.util.Scanner;
 public class OneandTwo {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t--!=0)
+        int tc=sc.nextInt();
+        while(tc--!=0)
         {
             int n=sc.nextInt();
-            int arr[]=new int[n];
-            boolean found=true;
+            int t=0,a[];
+            a=new int[n];
             for(int i=0;i<n;i++)
             {
-                arr[i]=sc.nextInt();
-            }
-            long pre[]=new long[n];
-            pre[0]=arr[0];
-            for(int i=1;i<n;i++)
-            {
-                pre[i]=pre[i-1]*arr[i];
-            }
-            for(int i=0;i<n;i++)
-            {
-                if(pre[i]==pre[n-1]/pre[i])
+                a[i]=sc.nextInt();
+                if(a[i]==2)
                 {
-                    System.out.println(i+1);
-                    found=false;
-                    break;
+                    t++;
                 }
             }
-            if(found)
+            if(t%2==0)
             {
-                System.out.println(-1);
+                int sum=0;
+                for(int i=0;i<n;i++)
+                {
+                    if(a[i]==2)
+                    {
+                        sum++;
+                    }
+                    if(sum==t/2)
+                    {
+                        System.out.println(i+1);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                System.out.println("-1");
             }
         }
     }
